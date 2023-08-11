7c42b2690a580d5ed70017757e221aa79f0eaa10
Anith Vithushan <vithusan091999@gmail.com>
2023-08-11 12:28:37 +0530

    hero image adjusted

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -37,7 +37,7 @@ function Hero() {
         </p>
       </div>
       <div className={` flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
-        <img src={robot} alt="billing" className='w-[100%] h-[100%] relative z-[5] ' />
+        <img src={robot} alt="billing" className='w-[100%] h-[100%] md:h-[75vh] xl:h-[100%] relative z-[5] ' />
         <div className="absolute z-0 w-2/5 h-[35%] top-0 overflow-hidden pink__gradient " />
         <div className="absolute z-1 w-4/5 h-4/5 rounded-full overflow-hidden bottom-40 white__gradient " />
         <div className="absolute z-0 w-1/2 h-1/2 right-20 overflow-hidden bottom-20 blue__gradient " />
